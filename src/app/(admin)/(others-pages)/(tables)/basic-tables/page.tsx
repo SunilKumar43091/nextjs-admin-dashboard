aaff33f0aa3da86985f6640d875d88d1b3665f5d
SunilKumar43091 <kulwantkaurpabyal@gmail.com>
2026-06-12 12:13:24 +0530

    side bar issue fixed on mobile view

--- src/app/(admin)/(others-pages)/(tables)/basic-tables/page.tsx
+++ src/app/(admin)/(others-pages)/(tables)/basic-tables/page.tsx
@@ -2,7 +2,7 @@ import ComponentCard from "@/components/common/ComponentCard";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import BasicTableOne from "@/components/tables/BasicTableOne";
 import { Metadata } from "next";
-import React from "react";
+
 
 export const metadata: Metadata = {
   title: "Next.js Basic Table | TailAdmin - Next.js Dashboard Template",
--- src/components/ecommerce/CountryMap.tsx
+++ src/components/ecommerce/CountryMap.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 // import { VectorMap } from "@react-jvectormap/core";
 import { worldMill } from "@react-jvectormap/world";
 import dynamic from "next/dynamic";
